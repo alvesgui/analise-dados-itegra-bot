d87cbbdc275969e0ee2fbb5c02d7de43a310e92e
Guilherme Alves <guilherme.araujo@itegra.com.br>
2022-11-18 19:19:21 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bot de Teletuito de fazer uma Analise de Dados de uma empresa pelo RH. O usuário vai poder solicitar graficos ou dados para um bot no telegran de uma planilha disponibilizada no Google Driver de uma empresa ficticia. Com o objetivo de facilitar uma analise por parte do pessoal do RH da empresa.',
+    author='Guilherme Alves',
+    license='',
+)
